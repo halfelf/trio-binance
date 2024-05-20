516be40326a3ed0bede77524d411845a68c87733
Shu Wang <shu.wang@nautiluscapital.net>
2024-05-20 09:42:46 +0800

    no stupid get historical klines api and test
    Signed-off-by: Shu Wang <shu.wang@nautiluscapital.net>

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -1,9 +1,7 @@
 import trio
 import pytest_trio
 
-from datetime import datetime, timedelta
 from trio_binance.client import AsyncClient
-from trio_binance.enums import KLINE_INTERVAL_1HOUR
 
 
 async def test_exchange_info():
@@ -17,19 +15,3 @@ async def test_exchange_info():
         assert "quoteAsset" in symbols[0]
         assert "orderTypes" in symbols[0]
         assert "filters" in symbols[0]
-
-
-async def test_klines():
-    client = await AsyncClient.create()
-    async with client:
-        start = datetime(2022, 1, 1, 8)
-        end = datetime(2022, 1, 1, 12)
-        klines = await client.get_historical_klines(
-            "BTCUSDT",
-            KLINE_INTERVAL_1HOUR,
-            start_str=int(start.timestamp() * 1000),
-            end_str=int(end.timestamp() * 1000),
-        )
-        assert isinstance(klines, list)
-        assert isinstance(klines[0], list)
-        assert len(klines) == (end - start) / timedelta(hours=1) + 1
